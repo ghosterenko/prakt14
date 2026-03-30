c498763d94544041a39687314275486c41c65fca
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-30 10:54:17 +0500

    Update pr14.cpp

--- pr14.cpp
+++ pr14.cpp
@@ -23,44 +23,52 @@ struct ClubState {
 
 HANDLE Visitors[20], seeker, hSemaphore;
 ClubState club;
+int count = 0;
 
 VOID WINAPI threadVisitor() {
 	srand(time(NULL));
-	club.clients[0].arriveTick = GetTickCount64();
+	while (count <= 20)
+	{
+		club.clients[0].arriveTick = GetTickCount64();
+		club.clients[0];
+		DWORD wait = WaitForSingleObject(hSemaphore, 3000);
+		if (wait == WAIT_OBJECT_0) {
+			club.clients[0].startTick = GetTickCount64() - club.clients[0].arriveTick;
 
-	DWORD wait = WaitForSingleObject(hSemaphore, 3000);
-	if (wait == WAIT_OBJECT_0) {
-		club.clients[0].startTick = GetTickCount64();
-		
-		int waitTime = rand() % 5;
-		Sleep(waitTime * 1000);
-		ReleaseSemaphore(hSemaphore, 1, NULL);
-		
-		club.clients[0].endTick = GetTickCount64();
-		
-		club.clients[0].served = TRUE;
-		club.clients[0].timeout = FALSE;
-	}
-	else if(wait == WAIT_TIMEOUT){
-		club.clients[0].served = FALSE;
-		club.clients[0].timeout = TRUE;
+			int waitTime = rand() % 5;
+			Sleep(waitTime * 1000);
+			ReleaseSemaphore(hSemaphore, 1, NULL);
 
-		club.timeoutCount++;
+			club.clients[0].endTick = GetTickCount64() - club.clients[0].endTick;
+
+			club.clients[0].served = TRUE;
+			club.clients[0].timeout = FALSE;
+		}
+		else if (wait == WAIT_TIMEOUT) {
+			club.clients[0].served = FALSE;
+			club.clients[0].timeout = TRUE;
+
+			club.timeoutCount++;
+		}
 	}
 }
 
 VOID WINAPI threadSeeker() {
-	while (true)
+	
+	while (count <= 20)
 	{
 		Sleep(500);
 		std::cout << club.currentVisitors << std::endl;
 		std::cout << club.servedCount << std::endl;
 		std::cout << club.timeoutCount << std::endl;
+		count++;
 	}
 }
 
 int main()
 {
+	hSemaphore = CreateSemaphoreA(NULL, 1, 3, NULL);
+
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
 		Visitors[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)threadVisitor, NULL, NULL, &club.clients[i].threadId);
@@ -85,6 +93,7 @@ int main()
 	if (!SetThreadPriority(seeker, THREAD_PRIORITY_LOWEST))
 		return GetLastError();
 
+	while (count <= 20) {}
 	CloseHandle(Visitors);
 	CloseHandle(seeker);
 	CloseHandle(hSemaphore);
